dbe96f0e9f39b06cc29fe930a4374edfdbd224f2
ParkJeongHun <mizz1030@naver.com>
2025-11-28 10:05:51 +0900

    feat: Swagger 추가

---
+++ src/main/java/com/ssafy/foody/common/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.ssafy.foody.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-key"))
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Foody API Documentation")
+                .description("Foody 프로젝트 API 명세서입니다.")
+                .version("1.0.0");
+    }
+}
