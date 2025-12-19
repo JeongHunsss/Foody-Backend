0e97804f2ca9d63ee5b6a47f1645e37a1fcd2f64
ParkJeongHun <mizz1030@naver.com>
2025-12-19 22:51:28 +0900

    refactor: validation message 수정

--- src/main/java/com/ssafy/foody/food/dto/FoodRequest.java
+++ src/main/java/com/ssafy/foody/food/dto/FoodRequest.java
@@ -9,20 +9,20 @@ import lombok.*;
 @Builder
 public class FoodRequest {
 
-    @NotBlank(message = "code는 필수입니다.")
-    @Size(max = 50, message = "code는 45자 이하여야 합니다.")
+    @NotBlank(message = "음식 코드는 필수입니다.")
+    @Size(max = 50, message = "음식 코드는 45자 이하여야 합니다.")
     private String code;
 
-    @NotBlank(message = "name은 필수입니다.")
-    @Size(max = 30, message = "name은 30자 이하여야 합니다.")
+    @NotBlank(message = "음식 이름은 필수입니다.")
+    @Size(max = 30, message = "음식 이름은 30자 이하여야 합니다.")
     private String name;
 
-    @NotBlank(message = "category는 필수입니다.")
-    @Size(max = 30, message = "category는 30자 이하여야 합니다.")
+    @NotBlank(message = "카테고리는 필수입니다.")
+    @Size(max = 30, message = "카테고리는 30자 이하여야 합니다.")
     private String category;
 
-    @NotBlank(message = "standard는 필수입니다.")
-    @Size(max = 10, message = "standard는 50자 이하여야 합니다.")
+    @NotBlank(message = "기준량은 필수입니다.")
+    @Size(max = 10, message = "기준량은 50자 이하여야 합니다.")
     private String standard;
 
     @PositiveOrZero(message = "kcal은 0 이상이어야 합니다.")
