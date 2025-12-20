9537bb37c9bc9452f60674cdf1b3b4fead0c13b9
ParkJeongHun <mizz1030@naver.com>
2025-12-20 19:30:39 +0900

    chore: 웹 소켓 권한 허용

--- src/main/java/com/ssafy/foody/auth/config/SecurityConfig.java
+++ src/main/java/com/ssafy/foody/auth/config/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
 						.requestMatchers("/admin/**").hasRole("ADMIN")
 						// 권한 없이 주소 허용
 						.requestMatchers("/error/**", "/account/**", "/oauth2/**", "/email/**", "/login/**", "/food/**",
-								"/favicon.ico", "/character/**")
+								"/favicon.ico", "/character/**", "/ws-stomp/**")
 						.permitAll()
 						// 권한 필요
 						.anyRequest().authenticated())
